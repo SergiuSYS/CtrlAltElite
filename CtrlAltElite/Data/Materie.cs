c78976fafdadfdd97c4d89948c19dd58e38a453a
Copil Alina <copilalinarodica17@gmail.com>
2025-04-04 20:36:12 +0300

    Clasa Materie

---
+++ CtrlAltElite/Data/Materie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CtrlAltElite.Data
+{
+    public class Materie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nume { get; set; }
+        public string Descriere { get; set; }
+        [Required]
+        public string Profesor { get; set; } 
+        
+    }
+}
